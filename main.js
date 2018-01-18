cc4de099ad0de9042d5dd27c02f608e14af18dd0
laurent <htd.laurent@gmail.com>
2018-01-18 16:42:35 +0100

    define goldenlayout service

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
 (function () {
   angular.module('app.spinal-pannel')
-    .run(["$templateCache", "$http", function ($templateCache, $http) {
-      let load_template = (uri, name) => {
-        $http.get(uri).then((response) => {
-          $templateCache.put(name, response.data);
-        }, (errorResponse) => {
-          console.log('Cannot load the file ' + uri);
-        });
-      };
-      let toload = [{
-        uri: '../templates/spinal-env-drive-pannel-logs/log-pannel.html',
-        name: 'log-pannel.html'
-      }];
-      for (var i = 0; i < toload.length; i++) {
-        load_template(toload[i].uri, toload[i].name);
-      }
-
-      goldenLayoutService.registerPannel({
-        id: "drag-folder-explorer",
-        name: "Log",
-        cfg: {
-          isClosable: true,
-          title: "Log",
-          type: 'component',
-          width: 20,
-          componentName: 'SpinalHome',
-          componentState: {
-            template: 'log-pannel.html',
-            module: 'app.spinal-pannel',
-            controller: 'InspectorCtrl'
-          }
+    .run(["$templateCache", "$http", "goldenLayoutService",
+      function ($templateCache, $http, goldenLayoutService) {
+        let load_template = (uri, name) => {
+          $http.get(uri).then((response) => {
+            $templateCache.put(name, response.data);
+          }, (errorResponse) => {
+            console.log('Cannot load the file ' + uri);
+          });
+        };
+        let toload = [{
+          uri: '../templates/spinal-env-drive-pannel-logs/log-pannel.html',
+          name: 'log-pannel.html'
+        }];
+        for (var i = 0; i < toload.length; i++) {
+          load_template(toload[i].uri, toload[i].name);
         }
-      })
+
+        goldenLayoutService.registerPannel({
+          id: "drag-folder-explorer",
+          name: "Log",
+          cfg: {
+            isClosable: true,
+            title: "Log",
+            type: 'component',
+            width: 20,
+            componentName: 'SpinalHome',
+            componentState: {
+              template: 'log-pannel.html',
+              module: 'app.spinal-pannel',
+              controller: 'InspectorCtrl'
+            }
+          }
+        })
 
 
-    }])
+      }
+    ])
     .controller('InspectorCtrl', ["$scope", "$injector", "authService", "$mdToast", "$interval", "$timeout",
       function ($scope, $injector, authService, $mdToast, $interval, $timeout) {
         $scope.injector = $injector;
